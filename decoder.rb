cc84dbd1e4f015dc848ddb9f5d11c5dd512ca807
Baroka <birotori@gmail.com>
2022-07-11 20:09:40 +0100

    Fix rubocop reports

--- decoder.rb
+++ decoder.rb
@@ -56,7 +56,7 @@ def split_world(phrase)
   if phrase.is_a? String
     phrase.split('   ')
   else
-    put "Your inpu should be a string in double or single quote !"
+    put 'Your inpu should be a string in double or single quote !'
   end
 end
 
@@ -64,21 +64,22 @@ end
 # takes a string parameter, and return the string representation.
 
 def decode(phrase)
-  worlds = split_world (phrase)
+  worlds = split_world(phrase)
   phrase_decode = ''
   worlds.each do |world|
     world_decode = ''
-    world.split(' ').each do |morse|
-      world_decode += decode_char(morse)
+    world.split().each do |morse|
+      dc = decode_char(morse)
+      world_decode += "#{dc}"
     end
-    phrase_decode += world_decode + ' '
+    phrase_decode += "#{world_decode} "
   end
   phrase_decode
 end
 
 # Testing
-decode("-- -.--   -. .- -- .")
+decode('-- -.--   -. .- -- .')
 
 # => MY NAME
-phrase_2 = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+phrase_2 = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 print decode(phrase_2) # => A BOX FULL OF RUBIES
