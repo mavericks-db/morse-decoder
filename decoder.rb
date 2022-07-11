5b6f624f5d05a2422dbd6a56634b9818c4189866
Baroka <birotori@gmail.com>
2022-07-11 19:54:53 +0100

    Add decode function

--- decoder.rb
+++ decoder.rb
@@ -50,3 +50,35 @@ end
 
 x = '.-'
 puts decode_char(x)
+
+# split sentence in worlds
+def split_world(phrase)
+  if phrase.is_a? String
+    phrase.split('   ')
+  else
+    put "Your inpu should be a string in double or single quote !"
+  end
+end
+
+# Create a method to decode an entire word in Morse code,
+# takes a string parameter, and return the string representation.
+
+def decode(phrase)
+  worlds = split_world (phrase)
+  phrase_decode = ''
+  worlds.each do |world|
+    world_decode = ''
+    world.split(' ').each do |morse|
+      world_decode += decode_char(morse)
+    end
+    phrase_decode += world_decode + ' '
+  end
+  phrase_decode
+end
+
+# Testing
+decode("-- -.--   -. .- -- .")
+
+# => MY NAME
+phrase_2 = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+print decode(phrase_2) # => A BOX FULL OF RUBIES
